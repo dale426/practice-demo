c801f6882d4d13b8269889817b07a74c2c752e24
long <yulongli>
2018-04-04 19:01:39 +0800

    删除 nuxt

--- nuxt/mynuxt/store/user-test.js
+++
@@ -1,9 +0,0 @@
-// import Cookies from 'js-cookie';
-export const state = () => ({
-    test: 'test-vuex'
-})
-export const mutations = {
-    logout (state, vm) {
-        console.log('arguments',...arguments)
-    }
-}
